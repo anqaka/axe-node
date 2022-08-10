796ebe051779572828b8c5d937416854d35849e9
Robert Woźniak <cookiecodestudio@gmail.com>
2022-08-10 10:02:50 +0200

    Add helpers entrypoint, simplify project index file

---
+++ src/helpers/index.ts
@@ -0,0 +1,6 @@
+export { default as axeTest } from './axe-test';
+export { default as configure } from './configure';
+export { default as getConfig } from './get-config';
+export { default as prepareIssues } from './prepare-issues';
+export { default as writeData } from './write-data';
+export { default as writeToCsv } from './result-to-csv';
